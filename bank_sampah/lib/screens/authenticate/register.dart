9ef624ebffd6a02e97f20a7c9188aa2f4fa059a2
rafinas2133 <90997180+rafinas2133@users.noreply.github.com>
2024-01-12 02:11:56 +0700

    overflow fix

--- bank_sampah/lib/screens/authenticate/register.dart
+++ bank_sampah/lib/screens/authenticate/register.dart
@@ -33,167 +33,169 @@ class _RegisterState extends State<Register> {
         padding: EdgeInsets.fromLTRB(20.0, 2.0, 20.0, 0.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // Gambar Logo
-              Container(
-                width: 450.0,
-                height: 50.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/bank_sampah_logo.jpeg'), // Gantilah dengan path gambar logo Anda
-                    fit: BoxFit.contain,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Gambar Logo
+                Container(
+                  width: 450.0,
+                  height: 50.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/bank_sampah_logo.jpeg'), // Gantilah dengan path gambar logo Anda
+                      fit: BoxFit.contain,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 10.0),
-          
-              Text(
-                'SIGN UP',
-                style:TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.green,
-                  fontWeight: FontWeight.bold
-                ),
-              ),
-          
-              // Input Nama
-              TextFormField(
-                decoration: textInputDecoration.copyWith(labelText: 'Full Name'),
-                validator: (val) => val!.isEmpty ? 'Enter Full Name' : null,
-                onChanged: (val) {
-                  setState(() {
-                    fullName = val;
-                  });
-                },
-              ),
-              SizedBox(height: 8.0),
-          
-              // Input username
-              TextFormField(
-                decoration: textInputDecoration.copyWith(labelText: 'Username'),
-                validator: (val) => val!.isEmpty ? 'Enter Username' : null,
-                onChanged: (val) {
-                  setState(() {
-                    userName = val;
-                  });
-                },
-              ),
-              SizedBox(height: 8.0),
-          
-              // Input NoHandPhone
-              TextFormField(
-                decoration: textInputDecoration.copyWith(labelText: 'No.Handphone'),
-                validator: (val) => val!.isEmpty ? 'Enter No. Handphone' : null,
-                onChanged: (val) {
-                  setState(() {
-                    noHp = val;
-                  });
-                },
-              ),
-              SizedBox(height: 8.0),
-          
-              // Input Email
-              TextFormField(
-                decoration: textInputDecoration.copyWith(labelText: 'Email'),
-                validator: (val) => val!.isEmpty ? 'Enter Email' : null,
-                onChanged: (val) {
-                  setState(() {
-                    email = val;
-                  });
-                },
-              ),
-              SizedBox(height: 8.0),
-          
-              // Input Password
-              TextFormField(
-                obscureText: true,
-                decoration: textInputDecoration.copyWith(labelText: 'Password'),
-                validator: (val) => val!.length < 8 ? 'Password must be 8+ chars long' : null,
-                onChanged: (val) {
-                  setState(() {
-                    password = val;
-                  });
-                },
-              ),
-              SizedBox(height: 8.0),
-          
-              // Input confirm Password
-              TextFormField(
-                obscureText: true,
-                decoration: textInputDecoration.copyWith(labelText: 'Confirm Password'),
-                validator: (val) => val != password ? 'Enter a same password' : null,
-                onChanged: (val) {
-                  setState(() {
-                    confirmPassword = val;
-                  });
-                },
-              ),
-              SizedBox(height: 8.0),
-          
-              // Input Adress
-              TextFormField(
-                decoration: textInputDecoration.copyWith(labelText: 'Address'),
-                validator: (val) => val!.isEmpty ? 'Enter Address' : null,
-                onChanged: (val) {
-                  setState(() {
-                    address = val;
-                  });
-                },
-              ),
-              SizedBox(height: 8.0),
-          
-              // Tombol Sign up
-              ElevatedButton(
-                onPressed: () async {
-                  if(_formKey.currentState!.validate()){
-                    setState(() => loading = true);
-                    dynamic result = await _auth.registerWithEmailAndPassword(fullName,userName,noHp, email, password, address);
-                    if (result == null){
-                      setState(() {
-                        loading = false;
-                      });
-                      
-                    }
-                  }
-                
-                },
-                style: ButtonStyle(
-                  fixedSize: MaterialStateProperty.all(
-                    Size(380, 30.0), // Sesuaikan lebar dan tinggi sesuai kebutuhan
-                  ),
-                  shadowColor: MaterialStateProperty.all(Colors.white),
-                  overlayColor: MaterialStateProperty.all(Colors.green),
-                  backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 99, 157, 100)),
-                  surfaceTintColor: MaterialStateProperty.all(Colors.white)
-                ),
-                child: Text(
-                  'Sign Up',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0
-                  ),
-                ),
-              ),
-              SizedBox(height: 10.0),
-              // Teks Daftar
-              GestureDetector(
-                onTap: () {
-                  widget.toggleView();
-                },
-                child: Text(
-                  'Sudah memiliki akun? Login disini',
-                  style: TextStyle(
+                SizedBox(height: 10.0),
+            
+                Text(
+                  'SIGN UP',
+                  style:TextStyle(
+                    fontSize: 20.0,
                     color: Colors.green,
-                    fontSize: 18,
+                    fontWeight: FontWeight.bold
                   ),
                 ),
-              ),
-          
-          
-            ],
+            
+                // Input Nama
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(labelText: 'Full Name'),
+                  validator: (val) => val!.isEmpty ? 'Enter Full Name' : null,
+                  onChanged: (val) {
+                    setState(() {
+                      fullName = val;
+                    });
+                  },
+                ),
+                SizedBox(height: 8.0),
+            
+                // Input username
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(labelText: 'Username'),
+                  validator: (val) => val!.isEmpty ? 'Enter Username' : null,
+                  onChanged: (val) {
+                    setState(() {
+                      userName = val;
+                    });
+                  },
+                ),
+                SizedBox(height: 8.0),
+            
+                // Input NoHandPhone
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(labelText: 'No.Handphone'),
+                  validator: (val) => val!.isEmpty ? 'Enter No. Handphone' : null,
+                  onChanged: (val) {
+                    setState(() {
+                      noHp = val;
+                    });
+                  },
+                ),
+                SizedBox(height: 8.0),
+            
+                // Input Email
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(labelText: 'Email'),
+                  validator: (val) => val!.isEmpty ? 'Enter Email' : null,
+                  onChanged: (val) {
+                    setState(() {
+                      email = val;
+                    });
+                  },
+                ),
+                SizedBox(height: 8.0),
+            
+                // Input Password
+                TextFormField(
+                  obscureText: true,
+                  decoration: textInputDecoration.copyWith(labelText: 'Password'),
+                  validator: (val) => val!.length < 8 ? 'Password must be 8+ chars long' : null,
+                  onChanged: (val) {
+                    setState(() {
+                      password = val;
+                    });
+                  },
+                ),
+                SizedBox(height: 8.0),
+            
+                // Input confirm Password
+                TextFormField(
+                  obscureText: true,
+                  decoration: textInputDecoration.copyWith(labelText: 'Confirm Password'),
+                  validator: (val) => val != password ? 'Enter a same password' : null,
+                  onChanged: (val) {
+                    setState(() {
+                      confirmPassword = val;
+                    });
+                  },
+                ),
+                SizedBox(height: 8.0),
+            
+                // Input Adress
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(labelText: 'Address'),
+                  validator: (val) => val!.isEmpty ? 'Enter Address' : null,
+                  onChanged: (val) {
+                    setState(() {
+                      address = val;
+                    });
+                  },
+                ),
+                SizedBox(height: 8.0),
+            
+                // Tombol Sign up
+                ElevatedButton(
+                  onPressed: () async {
+                    if(_formKey.currentState!.validate()){
+                      setState(() => loading = true);
+                      dynamic result = await _auth.registerWithEmailAndPassword(fullName,userName,noHp, email, password, address);
+                      if (result == null){
+                        setState(() {
+                          loading = false;
+                        });
+                        
+                      }
+                    }
+                  
+                  },
+                  style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all(
+                      Size(380, 30.0), // Sesuaikan lebar dan tinggi sesuai kebutuhan
+                    ),
+                    shadowColor: MaterialStateProperty.all(Colors.white),
+                    overlayColor: MaterialStateProperty.all(Colors.green),
+                    backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 99, 157, 100)),
+                    surfaceTintColor: MaterialStateProperty.all(Colors.white)
+                  ),
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                // Teks Daftar
+                GestureDetector(
+                  onTap: () {
+                    widget.toggleView();
+                  },
+                  child: Text(
+                    'Sudah memiliki akun? Login disini',
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+            
+            
+              ],
+            ),
           ),
         ),
       ),
--- bank_sampah/lib/screens/authenticate/sign_in.dart
+++ bank_sampah/lib/screens/authenticate/sign_in.dart
@@ -30,116 +30,118 @@ class _SignInState extends State<SignIn> {
         padding: EdgeInsets.all(10.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // Gambar Logo
-              Container(
-                width: 450.0,
-                height: 130.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/bank_sampah_logo.jpeg'), // Gantilah dengan path gambar logo Anda
-                    fit: BoxFit.contain,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Gambar Logo
+                Container(
+                  width: 450.0,
+                  height: 130.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/bank_sampah_logo.jpeg'), // Gantilah dengan path gambar logo Anda
+                      fit: BoxFit.contain,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 10.0),
-          
-              // Input Email
-              TextFormField(
-                decoration: textInputDecoration.copyWith(labelText: 'Email'),
-                validator: (val) => val!.isEmpty ? 'Enter Email' : null,
-                onChanged: (val) {
-                  setState(() {
-                    email = val;
-                  });
-                },
-                
-              ),
-              SizedBox(height: 10.0),
-          
-              // Input Password
-              TextFormField(
-                obscureText: _hidePass,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                    icon: Icon(Icons.visibility),
-                    onPressed: () {
-                      setState(() => _hidePass = !_hidePass);
-                    },
-                  ),
+                SizedBox(height: 10.0),
+            
+                // Input Email
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(labelText: 'Email'),
+                  validator: (val) => val!.isEmpty ? 'Enter Email' : null,
+                  onChanged: (val) {
+                    setState(() {
+                      email = val;
+                    });
+                  },
+                  
                 ),
-                validator: (val) => val!.isEmpty ? 'Enter Password' : null,
-                onChanged: (val) {
-                  setState(() {
-                    password = val;
-                  });
-                },
-              ),
-              SizedBox(height: 10.0),
-          
-              // Tombol Sign In
-              ElevatedButton(
-                onPressed: () async {
-                  if(_formKey.currentState!.validate()){
-                    setState(() => loading = true,);
-                    dynamic result = await _auth.signInWithEmailAndPassword(email, password);
-                    if(result == null){
-                      setState(() {
-                        loading = false;
-                        error = 'couldnot match your username or password';
-                      });
-                      
+                SizedBox(height: 10.0),
+            
+                // Input Password
+                TextFormField(
+                  obscureText: _hidePass,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.visibility),
+                      onPressed: () {
+                        setState(() => _hidePass = !_hidePass);
+                      },
+                    ),
+                  ),
+                  validator: (val) => val!.isEmpty ? 'Enter Password' : null,
+                  onChanged: (val) {
+                    setState(() {
+                      password = val;
+                    });
+                  },
+                ),
+                SizedBox(height: 10.0),
+            
+                // Tombol Sign In
+                ElevatedButton(
+                  onPressed: () async {
+                    if(_formKey.currentState!.validate()){
+                      setState(() => loading = true,);
+                      dynamic result = await _auth.signInWithEmailAndPassword(email, password);
+                      if(result == null){
+                        setState(() {
+                          loading = false;
+                          error = 'couldnot match your username or password';
+                        });
+                        
+                      }
+            
                     }
-
-                  }
-                },
-                style: ButtonStyle(
-                  fixedSize: MaterialStateProperty.all(
-                    Size(400, 50.0), // Sesuaikan lebar dan tinggi sesuai kebutuhan
+                  },
+                  style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all(
+                      Size(400, 50.0), // Sesuaikan lebar dan tinggi sesuai kebutuhan
+                    ),
+                    shadowColor: MaterialStateProperty.all(Colors.white),
+                    overlayColor: MaterialStateProperty.all(Colors.green),
+                    backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 99, 157, 100)),
+                    surfaceTintColor: MaterialStateProperty.all(Colors.white)
                   ),
-                  shadowColor: MaterialStateProperty.all(Colors.white),
-                  overlayColor: MaterialStateProperty.all(Colors.green),
-                  backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 99, 157, 100)),
-                  surfaceTintColor: MaterialStateProperty.all(Colors.white)
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0
+                    ),
+                  ),
                 ),
-                child: Text(
-                  'Sign In',
+                SizedBox(height: 10.0),
+                // Teks Daftar
+                GestureDetector(
+                  onTap: () {
+                    widget.toggleView();
+                  },
+                  child: Text(
+                    'Belum memiliki akun? Daftar disini',
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5.0,),
+                Text(
+                  error,
                   style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0
+                    color: Colors.red,
+                    fontSize: 14.0,
                   ),
-                ),
-              ),
-              SizedBox(height: 10.0),
-              // Teks Daftar
-              GestureDetector(
-                onTap: () {
-                  widget.toggleView();
-                },
-                child: Text(
-                  'Belum memiliki akun? Daftar disini',
-                  style: TextStyle(
-                    color: Colors.green,
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-              SizedBox(height: 5.0,),
-              Text(
-                error,
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 14.0,
-                ),
-              )
-          
-          
-            ],
+                )
+            
+            
+              ],
+            ),
           ),
         ),
       ),
